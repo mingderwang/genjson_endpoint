649b7ef021589304d8f5a918b9db3a8a6f67077b
Ming-der Wang <ming@bimap.co>
2020-01-05 19:02:26 +0800

    dir could be a single file, also add ps1

--- test3.js
+++ test3.js
@@ -1,15 +1,16 @@
 const { resolve } = require("path");
-var shell = require('node-powershell');
+var shell = require("node-powershell");
 const { readdir } = require("fs-extra").promises;
 const fs = require("fs-extra");
 const Promise = require("bluebird");
 const encode = require("base-64").encode;
-const readdirp = require("readdirp");
 const fetch = require("isomorphic-fetch");
 const fileInfo = require("./utils/fileAttributes");
 const URL = "https://10.99.1.10:9200/win_index/_doc/?pipeline=attachment";
 const username = "admin";
 const password = "admin";
+
+const isWindows = false;
 
 let headers = new Headers();
 headers.set(
@@ -18,8 +19,15 @@ headers.set(
 );
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 async function* getFiles(dir) {
+  const stat = await fs.lstat(dir);
+    console.log("isFile:",stat.isFile(), dir);
+    if (stat.isFile()) {
+      yield dir
+    }
   const dirents = await readdir(dir, { withFileTypes: true });
   for (const dirent of dirents) {
+    console.log("dir:", dir);
+    console.log("dirent:", dirent);
     const res = resolve(dir, dirent.name);
     if (dirent.isDirectory()) {
       yield* getFiles(res);
@@ -29,15 +37,15 @@ async function* getFiles(dir) {
   }
 }
 
-
-var ps = new shell({
-    executionPolicy: 'bypass',
+if (isWindows) {
+  var ps = new shell({
+    executionPolicy: "bypass",
     noProfile: true
-});
-
+  });
+}
 
 (async () => {
-  for await (const f of getFiles("./utils")) {
+  for await (const f of getFiles("./utils/test/post.js")) {
     console.log(f);
     let stats = fileInfo(f);
     console.log(stats);
@@ -46,15 +54,16 @@ var ps = new shell({
     if (stats == null) {
       console.log("------- directory, return");
     }
+    stats["windowsInfo"] = ps1(f);
     try {
       const results = await Promise.map(
-        ["test"],
+        ["nothing_here"],
         number =>
           fetch(URL, {
             method: "post",
             body: JSON.stringify(stats),
             headers: new Headers({
-              'Content-Type': 'application/json',
+              "Content-Type": "application/json",
               Authorization: "Basic " + encode(username + ":" + password)
             })
           }).then(res => console.log(res)),
@@ -68,9 +77,18 @@ var ps = new shell({
   }
 })();
 
- ps.addCommand("./getFileAcl.ps1", [ {
-        name: 'filePath', value: "." 
-    } ])
+const ps1 = filePath => {
+  if (isWindows) {
+    ps.addCommand("./getFileAcl.ps1", [
+      {
+        name: "filePath",
+        value: filePath
+      }
+    ]);
     ps.invoke().then(output => {
-        console.log(output)
-    })
+      console.log(output);
+    });
+  } else {
+    console.log("---- windows ps1 return: ", filePath);
+  }
+};
